eea4769ea90036145dfcf8935791ca8d93614ecc
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-06-06 16:39:44 +0200

    seeder of categories

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category; 
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +13,13 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categories = ['frontend', 'backend', 'design', 'ux', 'devops']; 
+        for($i = 0; $i < count($categories); $i++){
+            $newCategory = new Category();
+            $newCategory->name = $categories[$i]; 
+            $newCategory->slug = Category::generateSlug($newCategory->name); 
+            $newCategory->save();  
+
+        }
     }
 }
